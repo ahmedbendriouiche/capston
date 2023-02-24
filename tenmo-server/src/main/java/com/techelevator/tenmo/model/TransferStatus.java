320e3a8b6ea0e3d46bbb89ca713555823fad33d8
Dustin <slvrshadow89@gmail.com>
2023-02-24 16:11:15 -0500

    Added NotNull annotations to TransferStatus variables, as well as a default constructor and an overload constructor.

--- tenmo-server/src/main/java/com/techelevator/tenmo/model/TransferStatus.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/TransferStatus.java
@@ -1,8 +1,20 @@
 package com.techelevator.tenmo.model;
 
+import javax.validation.constraints.NotNull;
+
 public class TransferStatus {
+
+    @NotNull
     private int transferStatusId;
+    @NotNull
     private String transferStatusDesc;
+
+    public TransferStatus() {}
+
+    public TransferStatus(int transferStatusId, String transferStatusDesc) {
+        this.transferStatusId = transferStatusId;
+        this.transferStatusDesc = transferStatusDesc;
+    }
 
     public int getTransferStatusId() {
         return transferStatusId;
