645482dab1872ad59833560ed4e0163f333977a7
Dustin <slvrshadow89@gmail.com>
2023-03-02 02:23:54 -0500

    Reconfigured TransferStatusController. Code should account for multiple scenarios now.

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferStatusController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferStatusController.java
@@ -1,10 +1,10 @@
 package com.techelevator.tenmo.controller;
 
+import com.techelevator.tenmo.model.TransferStatus;
 import com.techelevator.tenmo.services.TransferStatusService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -32,17 +32,53 @@ public class TransferStatusController {
     @GetMapping(path = "")
     public ResponseEntity getStatus(@RequestParam(defaultValue = "0") long id,
                                     @RequestParam(required = false) String name) {
-        if (name != null && id > 0) {
-            return ResponseEntity.badRequest().body("Multiple inputs found. Please search for either a status id or " +
-                    "description.");
+        ResponseEntity response = null;
+        int validationCode = validateParameters(id, name);
+
+        if (validationCode == 1 || validationCode == 2)
+            response =  generateResponseWithError(validationCode);
+        if (validationCode == 3 || validationCode == 4)
+            response =  generateResponseWithTransferStatus(validationCode, id, name);
+
+        return response;
+    }
+
+    private int validateParameters(long id, String name) {
+        int validationCode;
+
+        if (name != null && id > 0) validationCode = 1;         /* Multiple inputs */
+        else if (name == null && id == 0) validationCode = 2;   /* No inputs */
+        else if (name != null) validationCode = 3;              /* Search by name */
+        else validationCode = 4;                                /* Search by id */
+
+        return validationCode;
+    }
+
+    // Generates a response letting the user know that they need to either add a parameter or use only one
+    private ResponseEntity generateResponseWithError(int code) {
+        ResponseEntity response = null;
+
+        if (code == 1) response =
+                ResponseEntity.badRequest()
+                        .body("Multiple inputs found. Please search for either a status id or description.");
+        if (code == 2) response =
+                ResponseEntity.badRequest()
+                        .body("No input found. Please search for either a status id or description.");
+
+        return response;
+    }
+
+    // Generates a response containing a TransferStatus or letting the user know that a status wasn't found
+    private ResponseEntity generateResponseWithTransferStatus(int code, long id, String name) {
+        TransferStatus status = null;
+
+        if (code == 3) status = transferStatusService.getStatusByName(name);
+        if (code == 4) status =  transferStatusService.getStatusById(id);
+
+        if (status == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Status not found.");
+        } else {
+            return ResponseEntity.ok(status);
         }
-        if ((name == null && id == 0) || (name != null && id > 0)) {
-            return ResponseEntity.badRequest().body("No input found. Please search for either a status id or " +
-                    "description.");
-        }
-        if (name != null) {
-            return ResponseEntity.ok(transferStatusService.getStatusByName(name));
-        }
-        return ResponseEntity.ok(transferStatusService.getStatusById(id));
     }
 }
